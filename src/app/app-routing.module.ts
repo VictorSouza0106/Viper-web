f51227a6e8f3b61d4436d210a81adbaa2a39f7f5
VictorSouza_ <victorsouza0106@gmail.com>
2022-06-23 20:50:41 -0300

    Adding routes / clear hard coded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarouselScheduleComponent } from './components/carousel-schedule/carousel-schedule.component';
+import { ThreeLoaderComponent } from './components/three-loader/three-loader.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component: ThreeLoaderComponent,
+  },
+  {
+    path:'cards',
+    component: CarouselScheduleComponent,
+  },
+  {
+    path:"**",
+    pathMatch:'full',
+    component:ThreeLoaderComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IconComponent } from './components/icon/icon.component';
 import { ThreeLoaderComponent } from './components/three-loader/three-loader.component';
+import { CarouselScheduleComponent } from './components/carousel-schedule/carousel-schedule.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ScheduleDialogComponent } from './components/schedule-dialog/schedule-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IconComponent,
-    ThreeLoaderComponent
+    ThreeLoaderComponent,
+    CarouselScheduleComponent,
+    ScheduleDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatDialogModule,
     HttpClientModule
   ],
   providers: [],
